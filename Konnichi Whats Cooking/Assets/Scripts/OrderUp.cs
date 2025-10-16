03b12a8ebaec38d7bcc4fb3df19e9dbfc43ae017
codingGrease <jackson.leclaire@digipen.edu>
2025-10-16 07:52:23 -0700

    AUDIO HAS ARRIVE
    Italian Audio Added to In Game

---
+++ Konnichi Whats Cooking/Assets/Scripts/OrderUp.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class OrderUp : MonoBehaviour
+{
+    public Type Otype;
+    public float cooldown;
+    public float timer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
