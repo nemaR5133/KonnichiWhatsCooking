3f59a82cf5050fc7b458b9e90d75cd5b7ab20f32
nemaR5133 <145371930+nemaR5133@users.noreply.github.com>
2025-10-21 09:07:31 -0700

    Animations + Sprites
    There WILL BE merge conflicts; I had to recreate the code to move the player. Dante said this was okay to do, and I have the original code backed up on a notepad if we want to go back.
    
    Not all sprites are added; I will wait to add any more until this project is in a more completed state.

--- Konnichi Whats Cooking/Assets/Scripts/PlayerController.cs
+++ Konnichi Whats Cooking/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
-/*
- * Name: Ryan Scheppler
- * Date: 9/25/24
- * Desc: 2d movement for a top down game
- */
+/*******************************************************************
+ * File name: PlayerController
+ * Author: Nathen Mattis
+ * Digipen Email: 1119065@lwsd.org
+ * Course: Game100
+ * 
+ * Description: Moves player on input, as well as animates the player
+ ********************************************************************/
 
 using System.Collections;
 using System.Collections.Generic;
@@ -11,10 +14,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Tooltip("Acceleration")]
-    public float accel = 100;
+    public float playerSpeed = 100;
+    public Animator anim;
 
-    private Vector3 inputMovement = Vector3.zero;
+    private Vector2 moveDirection;
+    private Vector2 lastMoveDirection;
     private Rigidbody2D myRb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,13 +29,47 @@ public class PlayerController : MonoBehaviour
     // FixedUpdate is called once per physics update
     private void FixedUpdate()
     {
-        myRb.AddForce( inputMovement.normalized *  accel );
+        Move();
+        Animate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputMovement.x = Input.GetAxisRaw("Horizontal");
-        inputMovement.y = Input.GetAxisRaw("Vertical");
+        ProcessInputs();
     }
+
+    // Processes the inputs given, doesn't actually move the player
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if((moveX == 0 && moveY == 0) && moveDirection.x != 0 || moveDirection.y != 0)
+        {
+            lastMoveDirection = moveDirection;
+        }
+
+        moveDirection = new Vector2(moveX, moveY);
+    }
+
+    // Actually moves the player's position
+    void Move()
+    {
+        myRb.velocity = new Vector2(moveDirection.x * playerSpeed, moveDirection.y * playerSpeed);
+    }
+
+    // Tells the Animator to change numbers to our current direction, then projects a different animation accordingly
+    void Animate()
+    {
+        anim.SetFloat("AnimMoveX", moveDirection.x);
+        anim.SetFloat("AnimMoveY", moveDirection.y);
+
+        anim.SetFloat("AnimMoveMagnitude", moveDirection.magnitude);
+
+        anim.SetFloat("AnimLastMoveX", lastMoveDirection.x);
+        anim.SetFloat("AnimLastMoveY", lastMoveDirection.y);
+        //anim.SetFloat("isMoving", true);
+    }
+
 }
